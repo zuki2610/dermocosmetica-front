654ced539cfc9a6b4cb93885be4cb5e5f642cf24
Isabel Palacios <issabelpalacios@gmail.com>
2023-02-27 13:08:12 -0300

    FIX Style pdf

--- src/Components/PDFDocument.jsx
+++ src/Components/PDFDocument.jsx
@@ -7,16 +7,14 @@ const PDFDocument = ({ paciente, recomendaciones, medicinas }) => {
     console.log('medicinas', medicinas);
  
     const styles = StyleSheet.create({
-      page: { backgroundColor: 'red' },
-      section: { color: 'white', textAlign: 'center', margin: 30, backgroundImage: URL("") }
+      page: { backgroundColor: 'white' },
+      section: { color: 'black', textAlign: 'center', margin: 30},
     });
- 
- 
     return (
     <Document>
       <Page size="A4" style={styles.page}>
-        <View style={styles.section}>>
-          <Text>Nombre: {paciente.name}</Text>
+        <View style={styles.section}>
+          <Text>Nombre: {paciente.nombre}</Text>
           <Text>RUT: {paciente.rut}</Text>
           <Text>Correo: {paciente.email}</Text>
           <Text>Fecha de nacimiento: {paciente.fechaNacimiento}</Text>
@@ -30,7 +28,7 @@ const PDFDocument = ({ paciente, recomendaciones, medicinas }) => {
           <View>
             {medicinas.map((medicina, index) => (
               <View key={index}>
-                <Image src={medicina.imagenUrl} />
+                <Image style={{ height: '15vh', width:"19vw" }} src={medicina.imagenUrl} />
                 <Text>Nombre: {medicina.name}</Text>
                 <Text>Marcas: {medicina.marcaId.name}</Text>
                 <Text>Componentes: {medicina.componente.join(', ')}</Text>
